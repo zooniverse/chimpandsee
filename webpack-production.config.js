f740714282264eff956b04ad086dd0da0824d49a
Sarah Allen <sarah@zooniverse.org>
2014-12-08 14:27:01 -0600

    webpack production config

---
+++ webpack-production.config.js
@@ -0,0 +1,47 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  watch: true,
+  context: __dirname + '/app',
+  entry: {
+    main: './main.cjsx'
+  },
+  output: {
+    path: path.join(__dirname, '/public/build', '[hash]'),
+    publicPath: '/public/build/[hash]/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.cjsx', '.coffee', '.styl']
+  },
+  module: {
+    loaders: [
+      { test: /\.cjsx$/, loaders: ['coffee-loader', 'cjsx-loader'] },
+      { test: /\.coffee$/, loader: 'coffee-loader' },
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+      { test: /\.png$/, loader: "url-loader?mimetype=image/png" },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.svg$/, loader: "file-loader" }
+    ],
+    noParse: [
+      /^react$/
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  watchDelay: 0,
+  devtool: 'eval-source-map',
+  plugins: [
+    function() {
+      this.plugin("done", function(stats) {
+        require("fs").writeFileSync(
+          path.join(__dirname, "public/build", "stats.json"),
+          JSON.stringify(stats.toJson())
+        );
+      });
+    }
+  ]
+};
